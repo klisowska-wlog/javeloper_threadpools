7968e97f3c4772606f45993ed35426095f73a036
klisowska-wlog <86469730+klisowska-wlog@users.noreply.github.com>
2022-09-29 13:17:14 +0200

    Added Main app for presentation myth

---
+++ src/main/java/com/pl/jellytech/javeloper/MainApp.java
@@ -0,0 +1,15 @@
+package com.pl.jellytech.javeloper;
+
+/**
+ * Javeloper main app
+ *
+ */
+public class MainApp
+{
+    public static void main( String[] args ) {
+        System.out.println( "Welcome to the Javeloper conference!" );
+
+
+        System.out.println("Goodbye!");
+    }
+}
